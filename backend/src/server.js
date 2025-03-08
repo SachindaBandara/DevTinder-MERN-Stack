3c9ce843fe3b8695f601098cc9f9422804c06003
SachindaBandara <sachindaudeni192@gmail.com>
2025-03-08 20:05:39 +0530

    implemented database connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const dbConnection = require("./config/dbConnection");
 
 const app = express();
 
-// app.use("/", (req, res) => {
-//     res.send("Hello World! 205");
-// });
+dbConnection()
+    .then(()=>{
+        console.log("Connected Database Successfully");
+        // PORT checking 
+        app.listen(5000, () => {
+            console.log("Server is running on port 5000");
+        });
+    })
+    .catch((err)=> {
+        console.log("Error in connecting database", err);
+    })
 
 
-app.listen(5500, () => {
-    console.log("Server is running on port 5500");
-});
